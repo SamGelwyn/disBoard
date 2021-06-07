9c18daba95b969e9403a5d8087316c22f229eb3e
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-06-07 13:40:18 +1000

    is a board

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import ChessBoard from 'chessboardjsx'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+       <ChessBoard/> 
       </header>
     </div>
   );
